796467cff3c46b4dce562fd9dee66c00d9760004
garywangop <garywangop@gmail.com>
2023-04-02 12:04:41 -0400

    Initialize project

---
+++ src/test/java/com/icims/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icims.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
